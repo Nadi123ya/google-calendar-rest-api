40e9764360ffad34b714fe4a8f66b846c04734a8
Nadi123ya <nadezhda.bdarenko.96@gmail.com>
2022-10-13 12:14:27 +0400

    commited

--- scripts/common/storage.js
+++
@@ -1,9 +0,0 @@
-export const setItem = (key, value) => {
-    localStorage.clear()
-    localStorage.setItem(key, JSON.stringify(value))
-}
-export const getItem = (key) => JSON.parse(localStorage.getItem(key))
-
-export const getDisplayedWeekStart = () => {
-    return new Date(JSON.parse(localStorage.getItem('displayedWeekStart')))
-}
